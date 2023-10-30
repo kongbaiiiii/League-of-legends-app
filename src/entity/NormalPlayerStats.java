11484bd2b508243f9622dfa6b76f2e16b585e80f
David Fan <qixiangfan03@gmail.com>
2023-10-30 14:23:51 -0400

    playerstats entity

--- src/entity/NormalPlayerStats.java
+++ src/entity/NormalPlayerStats.java
@@ -1,4 +1,28 @@
 package entity;
 
-public class NormalPlayerStats {
+import java.util.List;
+import java.util.Map;
+
+public class NormalPlayerStats implements PlayerStats{
+    private Matches recentMatches;
+
+    @Override
+    public Map<String, List<Float>> getKDAOvertime() {
+        return null;
+    }
+
+    @Override
+    public Map<String, Integer> getDamageOvertime() {
+        return null;
+    }
+
+    @Override
+    public Map<String, Integer> getGoldOvertime() {
+        return null;
+    }
+
+    @Override
+    public Map<String, Integer> getCSOvertime() {
+        return null;
+    }
 }
--- src/entity/PlayerStats.java
+++ src/entity/PlayerStats.java
@@ -1,4 +1,15 @@
 package entity;
 
+import java.util.List;
+import java.util.Map;
+
 public interface PlayerStats {
+    Map<String, List<Float>> getKDAOvertime();
+
+    Map<String, Integer> getDamageOvertime();
+
+    Map<String, Integer> getGoldOvertime();
+
+    Map<String, Integer> getCSOvertime();
+
 }
