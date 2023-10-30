d54bae4c7e7f68cc6546e9b46237bb2c72923d28
WOCHIMA <matthewchen2021@gmail.com>
2023-10-30 14:56:55 -0400

    match interface and match data class update

--- src/entity/Match.java
+++ src/entity/Match.java
@@ -1,4 +1,6 @@
 package entity;
 
 public interface Match {
+
+    public String getMatchID();
 }
--- src/entity/MatchData.java
+++ src/entity/MatchData.java
@@ -1,4 +1,47 @@
 package entity;
 
-public class MatchData {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+class MatchData implements Match {
+
+    private String matchid;
+
+    MatchData(String matchid, String opponentuserid, String gamemode, Long gameduration) {
+        this.matchid = matchid;
+        this.opponentuserid = opponentuserid;
+        this.gamemode = gamemode;
+        this.gameduration = gameduration;
+    }
+
+    @Override
+    public String getMatchID() {
+        return null;
+    }
+
+    private final List<String> teamMateUserID = new ArrayList<>() {
+    };
+
+    private String opponentuserid;
+
+    private final Map<String, List<Float>> kda = new HashMap<>() {
+    };
+
+    private final Map<String, String> champions = new HashMap<>();
+
+    private final Map<String, Integer> gold = new HashMap<>();
+
+    private final Map<String, Integer> damagetaken = new HashMap<>();
+
+    private String gamemode;
+
+    private Long gameduration;
+
+    private final Map<String, Integer> level = new HashMap<>();
+
+    private final Map<String, Integer> cs = new HashMap<>();
 }
+
+
