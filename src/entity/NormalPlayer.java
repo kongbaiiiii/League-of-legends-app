c1718ee110a277483f77ecd31e08da9e1270d907
David Fan <qixiangfan03@gmail.com>
2023-10-30 14:06:47 -0400

    player entity

--- src/entity/NormalPlayer.java
+++ src/entity/NormalPlayer.java
@@ -1,4 +1,21 @@
 package entity;
 
-public class NormalPlayer {
+public class NormalPlayer implements Player {
+    private final String userid;
+    private final String puuid;
+
+    public NormalPlayer(String userid, String puuid) {
+        this.userid = userid;
+        this.puuid = puuid;
+    }
+
+    @Override
+    public String getUserID() {
+        return userid;
+    }
+
+    @Override
+    public String getPuuiD() {
+        return puuid;
+    }
 }
