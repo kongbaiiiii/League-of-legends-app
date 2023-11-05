8c3336419816a2605abad4d06c9de416bdf94a11
David Fan <qixiangfan03@gmail.com>
2023-11-05 16:43:58 -0500

    PlayerDataAccessObject

---
+++ src/data_access/PlayerDataAccessObject.java
@@ -0,0 +1,99 @@
+package data_access;
+
+import entity.Player;
+import entity.PlayerFactory;
+import org.json.JSONException;
+import org.json.JSONObject;
+import use_case.login.LoginPlayerDataAccessInterface;
+
+import java.io.*;
+
+import okhttp3.*;
+
+
+public class PlayerDataAccessObject implements LoginPlayerDataAccessInterface {
+    private final File playerFile;
+
+    private final String authoKey;
+
+    private Player player;
+
+    private PlayerFactory playerFactory;
+
+    public PlayerDataAccessObject(String playerCSVPath, PlayerFactory playerFactory) throws IOException {
+        this.playerFactory = playerFactory;
+        playerFile = new File(playerCSVPath);
+        authoKey = getKey();
+
+        if (playerFile.length() == 0) {
+            savePlayer();
+        } else {
+            try (BufferedReader reader = new BufferedReader(new FileReader(playerFile))) {
+                String playerID = reader.readLine();
+                String puuid = reader.readLine();
+                this.player = this.playerFactory.create(playerID, puuid);
+            }
+        }
+    }
+
+    private String getKey() throws IOException {
+        File keyFile = new File("authoKey.csv");
+        try (BufferedReader reader = new BufferedReader(new FileReader(keyFile))) {
+            return reader.readLine();
+        }
+    }
+
+    private void savePlayer() {
+        BufferedWriter writer;
+        try {
+            writer = new BufferedWriter(new FileWriter(playerFile));
+            writer.write(player.getUserID());
+            writer.write(player.getPuuiD());
+            writer.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void saveKey() {
+        BufferedWriter writer;
+        File keyFile = new File("authoKey.csv");
+        try {
+            writer = new BufferedWriter(new FileWriter(keyFile));
+            writer.write(authoKey);
+            writer.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    @Override
+    public boolean validPlayerID(String playerID) {
+        OkHttpClient client = new OkHttpClient().newBuilder().build();
+        Request request = new Request.Builder()
+                .url(String.format("https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/%s/NA1", playerID))
+                .addHeader("X-Riot-Token", authoKey)
+                .build();
+        try {
+            Response response = client.newCall(request).execute();
+            assert response.body() != null;
+            JSONObject responseBody = new JSONObject(response.body().string());
+            responseBody.getString("puuid");
+            return true;
+        } catch (IOException | JSONException e) {
+            return false;
+        }
+    }
+
+    @Override
+    public void save(Player player) {
+        this.player = player;
+    }
+
+//    @Override
+//    public void setKey(String authoKey){
+//        this.authoKey =authoKey;
+//    }
+
+}
