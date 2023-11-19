583dd3f4bb7fa74d5349bb90b9a3f1f39bd2d445
David Fan <qixiangfan03@gmail.com>
2023-11-19 15:58:22 -0500

    view

---
+++ src/view/LoginView.java
@@ -0,0 +1,94 @@
+package view;
+
+import interface_adapter.login.LoginController;
+import interface_adapter.login.LoginState;
+import interface_adapter.login.LoginViewModel;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+
+public class LoginView extends JPanel implements ActionListener, PropertyChangeListener {
+    public final String viewName = "log in";
+    private final LoginViewModel loginViewModel;
+
+    final JTextField playerIDInputField = new JTextField(15);
+    private final JLabel playerIDErrorField = new JLabel();
+
+    final JButton logIn;
+    final JButton cancel;
+    private final LoginController loginController;
+
+    public LoginView(LoginViewModel loginViewModel, LoginController controller) {
+
+        this.loginController = controller;
+        this.loginViewModel = loginViewModel;
+        this.loginViewModel.addPropertyChangeListener(this);
+        JLabel title = new JLabel("Login Screen");
+        title.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        LabelTextPanel playerInfo = new LabelTextPanel(
+                new JLabel("PlayerID"), playerIDInputField);
+
+        JPanel buttons = new JPanel();
+        logIn = new JButton(loginViewModel.LOGIN_BUTTON_LABEL);
+        buttons.add(logIn);
+        cancel = new JButton(loginViewModel.CANCEL_BUTTON_LABEL);
+        buttons.add(cancel);
+
+        logIn.addActionListener(
+                new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        if (e.getSource().equals(logIn)) {
+                            LoginState currentState = loginViewModel.getState();
+
+                            loginController.execute(currentState.getPlayerID());
+                        }
+                    }
+                }
+        );
+
+        cancel.addActionListener(this);
+
+        playerIDInputField.addKeyListener(new KeyListener() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+                LoginState currentState = loginViewModel.getState();
+                currentState.setPlayerID(playerIDInputField.getText() + e.getKeyChar());
+                loginViewModel.setState(currentState);
+            }
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+
+            }
+        });
+        this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+        this.add(title);
+        this.add(playerInfo);
+        this.add(playerIDErrorField);
+        this.add(buttons);
+    }
+    @Override
+    public void actionPerformed(ActionEvent e) {System.out.println("Click " + e.getActionCommand());}
+
+    @Override
+    public void propertyChange(PropertyChangeEvent evt) {
+        LoginState state = (LoginState) evt.getNewValue();
+        setFields(state);
+    }
+    private void setFields(LoginState state) {
+        playerIDInputField.setText(state.getPlayerID());
+    }
+}
