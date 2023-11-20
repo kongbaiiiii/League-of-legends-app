a38ec05875161c2f8772ed6c42865c230eb6db31
Ja....ck <zhuorui.jiang@mail.utoronto.ca>
2023-11-20 13:22:46 -0500

    test view

--- src/test_view.java
+++ src/test_view.java
@@ -4,6 +4,7 @@ import java.awt.*;
 public class test_view {
 
     public static void main(String[] args) {
+        //n
 //        JFrame f = new JFrame();
 //        f.setVisible(true);
 //        f.setSize(600, 400);
