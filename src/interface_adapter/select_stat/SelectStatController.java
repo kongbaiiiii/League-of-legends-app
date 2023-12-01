b90dbb9591985f7e9d84f7f05da5d3ed0c56ccf8
Ja....ck <zhuorui.jiang@mail.utoronto.ca>
2023-11-30 23:36:18 -0500

    SelectStatController implemented

---
+++ src/interface_adapter/select_stat/SelectStatController.java
@@ -0,0 +1,16 @@
+package interface_adapter.select_stat;
+
+import use_case.select_stat.SelectStatInputData;
+import use_case.select_stat.SelectStatInteractor;
+
+public class SelectStatController {
+    private final SelectStatInteractor selectStatUseCaseInteractor;
+
+    public SelectStatController(SelectStatInteractor selectStatUseCaseInteractor) {
+        this.selectStatUseCaseInteractor = selectStatUseCaseInteractor;
+    }
+
+    public void excute(){
+        selectStatUseCaseInteractor.execute();
+    }
+}
